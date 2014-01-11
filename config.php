d2fd30c06fa600a8b52e1313d72caeb87e324fee
Tom Canac <contact@tomcanac.com>
2014-01-11 16:41:31 +0100

    rounded : better folder names

--- config.php
+++ config.php
@@ -19,8 +19,8 @@ Please enjoy this free script!
 $thumbs_pr_page 		= "39"; //Number of thumbnails on a single page
 $gallery_width 			= "80%"; //Gallery width. Eg: "500px" or "70%"
 $backgroundcolor 		= "white"; //This provides a quick way to change your gallerys background to suit your website. Use either main colors like "black", "white", "yellow" etc. Or HEX colors, eg. "#AAAAAA"
-$templatefile 			= "squares"; //Template filename (must be placed in 'templates' folder)
-$title 					= "MiniGal Nano test gallery"; // Text to be displayed in browser titlebar
+$templatefile 			= "rounded"; //Template filename (must be placed in 'templates' folder)
+$title 					= "MiniBoardNano gallery"; // Text to be displayed in browser titlebar
 $author					= "John Doe";
 $folder_color 			= "black"; // Color of folder icons: blue / black / vista / purple / green / grey
 $sorting_folders		= "name"; // Sort folders by: [name][date]
@@ -39,5 +39,5 @@ $label_loading 			= "Loading..."; //Thumbnail loading text
 //ADVANCED SETTINGS
 $thumb_size 			= 320; //Thumbnail height/width (square thumbs). Changing this will most likely require manual altering of the template file to make it look properly! 
 $label_max_length 		= 40; //Maximum chars of a folder name that will be displayed on the folder thumbnail  
-$display_exif			= 0;
+$display_exif			= 0; //Take care, even if not diplayed EXIF are still readable for visitors. May be a good idea to erase EXIF datas...
 ?>
