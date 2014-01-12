ef74392a6f6e88555c660740862aec4179e289c4
Tom Canac <contact@tomcanac.com>
2014-01-12 14:09:31 +0100

    amélioration des thèmes

--- config.php
+++ config.php
@@ -19,7 +19,7 @@ Please enjoy this free script!
 $thumbs_pr_page 		= "39"; //Number of thumbnails on a single page
 $gallery_width 			= "80%"; //Gallery width. Eg: "500px" or "70%"
 $backgroundcolor 		= "white"; //This provides a quick way to change your gallerys background to suit your website. Use either main colors like "black", "white", "yellow" etc. Or HEX colors, eg. "#AAAAAA"
-$templatefile 			= "rounded"; //Template filename (must be placed in 'templates' folder)
+$templatefile 			= "board"; //Template filename (must be placed in 'templates' folder)
 $title 					= "MiniBoardNano gallery"; // Text to be displayed in browser titlebar
 $author					= "John Doe";
 $folder_color 			= "black"; // Color of folder icons: blue / black / vista / purple / green / grey
