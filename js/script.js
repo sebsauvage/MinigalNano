1d5ab274b26c6844989c7f5b7847046e07fab9e6
Tom Canac <contact@tomcanac.com>
2016-06-25 19:48:11 +0200

    Clean useless stuff

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
-/*=== lazyLoading ===*/
+/*
+ * LazyLoading
+ */
 window.addEventListener('load', function(){
 	var bLazy = new Blazy;
 }, false);
-
-/*=== Message close ===*/
-document.getElementById('closeMessage').addEventListener('click', function(){
-	document.getElementById('Message').style.display = 'none';
-});
