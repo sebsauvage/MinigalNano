cf26eddaaeee0814f055a6f5f1500063b5ded8ef
Tom Canac <tcanac@advisa.fr>
2016-06-27 12:40:07 +0200

    fix some php tags

--- templates/board/index.php
+++ templates/board/index.php
@@ -2,34 +2,34 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <meta name="author" content="<? echo $author ?>" />
-        <meta name="description" content="<? echo $description ?>" />
-        <meta name="generator" content="MinigalNano <? echo $version ?>" />
+        <meta name="author" content="<?php echo $author ?>" />
+        <meta name="description" content="<?php echo $description ?>" />
+        <meta name="generator" content="MinigalNano <?php echo $version ?>" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name='robots' content='index,follow'>
-        <title><? echo $title ?></title>
+        <title><?php echo $title ?></title>
         <link rel="alternate" type="application/rss+xml" title="<% title %>" href="rss.php" />
-        <link rel="stylesheet" href="<? echo THEME_ROOT ?>styles.css">
+        <link rel="stylesheet" href="<?php echo THEME_ROOT ?>styles.css">
     </head>
     <body>
         <header role="banner">
-            <h1><a href="<? echo GALLERY_ROOT ?>"><? echo $title ?></a></h1>
+            <h1><a href="<?php echo GALLERY_ROOT ?>"><?php echo $title ?></a></h1>
 
-            <nav role="navigation"><? echo $breadcrumb_navigation?></nav>
+            <nav role="navigation"><?php echo $breadcrumb_navigation?></nav>
         </header>
 
         <main>
-            <? if(count($folder_comment) > 0) :?>
-                <p><? echo $folder_comment ?></p>
-            <? endif; ?>
+            <?php if(count($folder_comment) > 0) :?>
+                <p><?php echo $folder_comment ?></p>
+            <?php endif; ?>
 
-            <? if(count($messages) > 0) :?>
+            <?php if(count($messages) > 0) :?>
                 <ul class="messages">
-                    <? foreach ($messages as $message) : ?>
-                        <li class="messages__message"><? echo $messages; ?></li>
-                    <? endforeach; ?>
+                    <?php foreach ($messages as $message) : ?>
+                        <li class="messages__message"><?php echo $message; ?></li>
+                    <?php endforeach; ?>
                 </ul>
-            <? endif; ?>
+            <?php endif; ?>
 
             <?php if ( count($images) > 0) : ?>
                 <ul>
@@ -49,24 +49,24 @@
             <?php endif ?>
         </main>
 
-        <nav role="navigation"><? echo $page_navigation ?></nav>
+        <nav role="navigation"><?php echo $page_navigation ?></nav>
 
         <a href="#top" id="backtop">top</a>
 
         <footer role="contentinfo">
-            Gallery by <? echo $author ?> /
+            Gallery by <?php echo $author ?> /
             <a href="https://github.com/sebsauvage/MinigalNano" title="Powered by MiniGal Nano" target="_blank">
-                Powered by MiniGal Nano <? echo $version ?>
+                Powered by MiniGal Nano <?php echo $version ?>
             </a> /
             <a href="http://tomcanac.com/minigal/" title="Tom Canac" target="_blank">
                 Board theme by Tom Canac
             </a> /
-            <a  title="<? echo $title ?> RSS" href="rss.php">
+            <a  title="<?php echo $title ?> RSS" href="rss.php">
                 RSS
             </a>
         </footer>
 
-        <script src="<? echo GALLERY_ROOT ?>js/lazy.js"></script>
-        <script src="<? echo THEME_ROOT ?>script.js"></script>
+        <script src="<?php echo GALLERY_ROOT ?>js/lazy.js"></script>
+        <script src="<?php echo THEME_ROOT ?>script.js"></script>
     </body>
 </html>
